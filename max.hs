2907d5adf8d8286089626923eaf050c326e07518
Gabriela Georgieva <gabriella.georgieva17@gmail.com>
2017-10-11 21:03:49 +0100

    Find max from list example

---
+++ max.hs
@@ -0,0 +1,10 @@
+-- find the max value in an Int list
+
+maxhelper :: Int -> [Int] -> Int
+maxhelper x [] = x
+maxhelper x (y:ys) = maxhelper
+  (if x>y then x else y) ys
+
+maxfromlist :: [Int] -> Maybe Int
+maxfromlist [] = Nothing
+maxfromlist (x:xs) = Just (maxhelper x xs)
